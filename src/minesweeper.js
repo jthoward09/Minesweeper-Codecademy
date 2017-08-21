10d7db6b01411e75344dc9ce444e49596edfdf43
James Howard <jthoward09@gmail.com>
2017-08-21 18:24:19 -0400

    Initial Add of Minesweeper Project

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+// Creates variable for blank line grid
+const blankLine = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| '
+
+// Console log how blank board will look
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Console log how filled board will look
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
