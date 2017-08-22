d29c2aa7541d746553200268d8f2aa615646cc55
James Howard <jthoward09@gmail.com>
2017-08-22 17:03:22 -0400

    [Codecademy] Updates Minesweeper with functions and arrays

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,19 @@
-// Creates variable for blank line grid
-const blankLine = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| '
+'use strict';
 
-// Console log how blank board will look
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const printBoard = board => {
+    console.log('Current Board: ');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
-// Console log how filled board will look
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+let board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
